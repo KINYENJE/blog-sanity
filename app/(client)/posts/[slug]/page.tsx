216d6d27c47ae66d2984d123a4f5cfa36899915c
James Njenga <njengajames232@gmail.com>
2024-02-05 23:38:10 +0300

    fetching tags and date from sanity

--- app/(client)/posts/[slug]/page.tsx
+++ app/(client)/posts/[slug]/page.tsx
@@ -1,7 +1,12 @@
 import Heading from '@/app/components/Heading'
 import { Post } from '@/app/utils/Interface'
 import { client } from '@/sanity/lib/client'
+import { VT323 } from 'next/font/google'
+import Link from 'next/link'
 import React from 'react'
+
+
+const dateFont = VT323({weight: "400", subsets: ['latin']})
 
 interface Params {
   params: {
@@ -33,15 +38,27 @@ const page = async({ params }: Params) => {
 
   return (
     <div>
-      <Heading title= "Article" />
+      <Heading title= {post[0]?.title} />
 
-      <h1>Article</h1>
+      
       {/** POST CONTENT */}
       {post?.length > 0 && post.map((p: Post) => {
         return (
-          <div key={p._id}>
-            <h2>{p.title}</h2>
+          <div key={p._id} className='text-center'>
             <p>{p.excerpt}</p>
+            <span className= {`${dateFont?.className} text-purple-500`}>
+              {new Date(p.publishedAt).toDateString()}
+            </span>
+
+            <div className="mt-5">
+          {p?.tags?.map((tag) => (
+            <Link key={tag?._id} href={`/tag/${tag.slug.current}`}>
+              <span className="mr-2 p-1 rounded-sm text-sm lowercase dark:bg-gray-950 border dark:border-gray-900">
+                #{tag.name}
+              </span>
+            </Link>
+          ))}
+        </div>
           </div>
         )
       })}
