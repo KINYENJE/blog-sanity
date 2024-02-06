5f1967f48ae54206a16434a7da237bbd82977890
James Njenga <njengajames232@gmail.com>
2024-02-06 17:46:58 +0300

    added studio link button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import ThemeSwitch from "./ThemeSwitch";
 import { Lilita_One } from "next/font/google";
+import { AddIcon } from "./Icons";
 
 const font = Lilita_One({ weight: "400", subsets: ["latin"] });
 
@@ -11,11 +12,18 @@ const Navbar = () => {
       <div className="flex justify-between items-center h-16 w-full">
         <Link href="/">
           <div className={`${font.className} text-3xl dark:text-amber-50`}>
-            Dev
-            <span className="text-purple-500">Blook</span>
+            X
+            <span className="text-purple-500">Blog</span>
           </div>
         </Link>
-        <ThemeSwitch />
+        <div className="flex gap-4">
+          <Link href="/studio">
+            <div className="border border-purple-500 rounded-2xl hover:bg-purple-500 hover:bg-opacity-10 dark:hover:bg-opacity-10 dark:hover:bg-amber-50 p-2">
+              <AddIcon />
+            </div>
+          </Link>
+          <ThemeSwitch />
+        </div>
       </div>
     </div>
   );
